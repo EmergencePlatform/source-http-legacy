4a801eb1757c3978f3c628328389b64ac33bcb75
Chris Alfano <chris@jarv.us>
2018-04-15 12:51:24 -0400

    Test get*FromEnvironment methods

--- pull.js
+++ pull.js
@@ -13,10 +13,12 @@ pull().catch(err => logger.error('Failed to start tool:', err.message));
 async function pull () {
     const git = await require('git-client').requireVersion('>=2.7.4');
 
-    let status = await git.status({ help: true });
+    //let status = await git.status({ help: true });
 
-    logger.debug('git status:', status);
+    //logger.debug('git status:', status);
     // logger.info('git status:', await git.status());
 
-    debugger;
+    logger.debug('getGitDirFromEnvironment:', await git.Git.getGitDirFromEnvironment());
+    logger.debug('getWorkTreeFromEnvironment:', await git.Git.getWorkTreeFromEnvironment());
+
 }
